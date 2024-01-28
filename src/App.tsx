f8cd2d8887e8bf7a9e92909f5d7de84cbd016957
Miltosh <skalinovskiy94@gmail.com>
2024-01-28 04:18:03 +0200

    changed paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react3'
+import { FC, useState, useEffect } from 'react'
 import { ModeSelector } from './components/ModeSelector/ModeSelector'
 import { Squares } from './components/Squares/Squares'
 import { SquaresInfo } from './components/SquaresInfo/SquaresInfo'

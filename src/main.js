d9af93beb5fc4d0e2434d7b9743b35c2201c594e
Miltosh <skalinovskiy94@gmail.com>
2024-01-28 04:14:36 +0200

    changed paths

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import ReactA from 'react2'
+import React from '/node_modules/react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  ReactA.createElement(ReactA.StrictMode, null, ReactA.createElement(App)),
+  React.createElement(React.StrictMode, null, React.createElement(App)),
 )
